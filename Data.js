d82c81276f763c17f0964c151d77fb0a02347653
Hexolen <107923345+Hexolen@users.noreply.github.com>
2022-06-21 13:28:29 +0300

    Add files via upload

---
+++ Data.js
@@ -0,0 +1,14 @@
+var players = [
+    {'no' : 1, 'alias':'Emre', 'rank' : 'gold'},
+    {'no' : 2, 'alias':'Efe', 'rank' : 'silver'},
+    {'no' : 3, 'alias':'Ali', 'rank' : 'plat'},
+];
+
+var characters=[
+    {pno : 1, 'character':'Althaea', 'class':'Cleric', 'level':'4'},
+    {pno : 2, 'character':'ECSA', 'class':'Fighter', 'level':'1'},
+    {pno : 2, 'character':'Sha\'Tra', 'class':'Wizard', 'level':'1'},
+    {pno : 3, 'character':'Sanior', 'class':'Cleric', 'level':'1'},
+    {pno : 3, 'character':'Sanior2', 'class':'Cleric', 'level':'1'},
+];
+
